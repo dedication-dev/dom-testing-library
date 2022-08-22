dc0fd944af842344008b94780b5bc5640959db68
Patrick Neulichedl <patrickneu@hotmail.de>
2022-08-22 18:20:38 +0200

    Implement IntoIterator for NodeList

--- web-sys-testing-library/src/dom/node_list.rs
+++ web-sys-testing-library/src/dom/node_list.rs
@@ -1,10 +1,26 @@
 #[derive(Clone, Debug, PartialEq)]
 pub struct NodeList {
     inner: web_sys::NodeList,
+}
+
+impl IntoIterator for NodeList {
+    type Item = web_sys::Node;
+    type IntoIter = IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Self::IntoIter {
+            inner: self.inner,
+            index: 0,
+        }
+    }
+}
+
+pub struct IntoIter {
+    inner: web_sys::NodeList,
     index: u32,
 }
 
-impl Iterator for NodeList {
+impl Iterator for IntoIter {
     type Item = web_sys::Node;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -16,9 +32,6 @@ impl Iterator for NodeList {
 
 impl From<web_sys::NodeList> for NodeList {
     fn from(node_list: web_sys::NodeList) -> Self {
-        Self {
-            inner: node_list,
-            index: 0,
-        }
+        Self { inner: node_list }
     }
 }
